b8cc395500c9bc0a39100781f5da7d3ee3b7f0a0
Brightwell-Dlamini <siyabongadlamini505@gmail.com>
2022-09-08 18:00:58 +0200

    continue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Header from '../components/Header';
+import Landing from '../components/Landing';
 
 const Home: NextPage = () => {
   return (
@@ -8,7 +10,10 @@ const Home: NextPage = () => {
         <title>Apple | Home</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <h1>Lets build the apple website</h1>
+      <Header />
+      <main>
+        <Landing />
+      </main>
     </div>
   );
 };
